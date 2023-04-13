c0f1d827d0f440900280fa739fa94bf197984aac
William Diangellis <diangellis2014@gmail.com>
2023-04-13 15:43:11 -0300

    emplementação do endpoint de listar categorias

--- dump.sql
+++ dump.sql
@@ -1,25 +1,19 @@
-create database pdv
+create database pdv;
 
-create table usuarios (
-    id serial primary key,
-    nome varchar 50 not null,
-    email text unique not null,
-    senha text not null
-)
+create table
+    usuarios (
+        id serial primary key,
+        nome varchar (50) not null,
+        email text unique not null,
+        senha text not null
+    );
 
-create table categorias (
-    id serial primary key,
-    descricao text
-)
+create table
+    categorias (
+        id serial primary key,
+        descricao text
+    );
 
-insert into 
-categorias (descricao) 
-values 
-('Informática'),
-('Celulares'),
-('Beleza e Perfumaria'),
-('Mercado'),
-('Livros e Papelaria'),
-('Moda'),
-('Bebê'),
-('Games')
+insert into
+    categorias (descricao)
+values ('Informática'), ('Celulares'), ('Beleza e Perfumaria'), ('Mercado'), ('Livros e Papelaria'), ('Moda'), ('Bebê'), ('Games');
