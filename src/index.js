fd68cf7904b887005c21120582fa3c2a01af0aaf
William Diangellis <diangellis2014@gmail.com>
2023-04-21 13:56:35 -0300

    Correção do response da rota cadastrar usuario e modificação no app.listen, favor verificar

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ const swaggerDoc = require('../swagger.json')
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT || 3030
+
 app.use(express.json())
 app.use('/api-docs', swagger.serve, swagger.setup(swaggerDoc))
 app.use(rotas)
 
+// app.listen(process.env.PORT, () => {
+//   console.log(`Servidor conectado a porta: ${process.env.PORT || 3030}`)
+// })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor conectado a porta: ${process.env.PORT || 3030}`)
+app.listen(port, () => {
+  console.log(
+    `Server running on port ${port}\nhttp://localhost:${port}\npress CTRL+C to stop server`
+  )
 })
