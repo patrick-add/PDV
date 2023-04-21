cabfa89764ef1219fc57899050326ee3dcc41570
Nilsooom <nilsooom.b@gmail.com>
2023-04-21 02:28:44 -0300

    Implemetação de documentação com Swagger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 
 const rotas = require('./rotas')
+const swagger = require('swagger-ui-express')
+const swaggerDoc = require('../swagger.json')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDoc))
 app.use(rotas)
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor conectado a porta: ${process.env.PORT || 3030}`)
