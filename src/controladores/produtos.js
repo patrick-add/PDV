281eac47f5cd703b5c856c67ad163d41821ceb42
KingOfParanaue <maateus.lopes.nv@hotmail.com>
2023-04-27 15:18:13 -0300

    Criação da função editarProdutos

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -1,49 +1,49 @@
-const knex = require('knex')
+const knex = require('../conexao')
 
-const cadastrarProduto = async(req, res) => {
-const {descricao, quantidade_estoque, valor, categoria_id} = req.body
+const cadastrarProduto = async (req, res) => {
+  const { descricao, quantidade_estoque, valor, categoria_id } = req.body
 
-if (categoria_id <=0 || categoria_id > 8){
-return res.status(404).json({mensagem: "A categoria informada não existe!"})
+  if (categoria_id <= 0 || categoria_id > 8) {
+    return res.status(404).json({ mensagem: "A categoria informada não existe!" })
+  }
+
+  try {
+    await knex('produtos').insert({
+      descricao, quantidade_estoque, valor, categoria_id
+    })
+
+    return res.status(201).json({ mensagem: "O produto foi cadastrado com sucesso!" })
+
+  } catch (error) {
+    return res.status(500).json({ mensagem: `Erro interno: ${error.menssage}` })
+  }
 }
 
-try {
-  await knex('produtos').insert({
-    descricao, quantidade_estoque, valor, categoria_id
-  })
+const listarProduto = async (req, res) => {
+  const { categoria_id } = req.query
 
-  return res.status(201).json({mensagem: "O produto foi cadastrado com sucesso!"})
-  
-} catch (error) {
-  return res.status(500).json({mensagem: `Erro interno: ${error.menssage}`})
-}
-}
+  try {
 
-const listarProduto = async(req, res) =>{
-    const {categoria_id} = req.query
+    if (categoria_id && Number(categoria_id) >= 1 && Number(categoria_id) <= 8) {
 
-try {
+      const categoriasDeProduto = await knex('produtos').where({ categoria_id })
 
-    if (categoria_id && Number(categoria_id) >=1 && Number(categoria_id) <=8){
-
-        const categoriasDeProduto = await knex('produtos').where({categoria_id})
-
-        if (!categoriasDeProduto){
- return res.status(404).json({
-    mensagem: "Não encotramos nenhum produto cadastrado para a categoria informada."
-})
-        } else {
-            return res.staus(200).json(categoriasDeProduto)
-        }
+      if (!categoriasDeProduto) {
+        return res.status(404).json({
+          mensagem: "Não encotramos nenhum produto cadastrado para a categoria informada."
+        })
+      } else {
+        return res.staus(200).json(categoriasDeProduto)
+      }
     }
 
     const listaDeProdutos = await knex('produtos')
 
     return res.status(200).json(listaDeProdutos)
-    
-} catch (error) {
-    return res.status(500).json({mensagem: `Erro interno: ${error.menssage}`})
-}
+
+  } catch (error) {
+    return res.status(500).json({ mensagem: `Erro interno: ${error.menssage}` })
+  }
 }
 
 const deletarProdutoPorId = async (req, res) => {
@@ -60,14 +60,42 @@ const deletarProdutoPorId = async (req, res) => {
 
     return res.status(200).json({ mensagem: 'Produto deletado com sucesso' })
   } catch (error) {
-    return res.status(500).json({mensagem: `Erro interno: ${error.menssage}`})
+    return res.status(500).json({ mensagem: `Erro interno: ${error.menssage}` })
+  }
+}
+
+// Verificar refatoração e reutilização com função de cadastrar produto
+// Verificar pontos de validação do schemaProdutos
+const editarDadosProduto = async (req, res) => {
+  const { id } = req.params
+  const { descricao, quantidade_estoque, valor, categoria_id } = req.body
+
+  try {
+    const verifiarPoduto = await knex('produtos').where({ id }).first();
+
+    if (!verifiarPoduto) {
+      return res.status(404).json({ mensagem: "Id informado não foi encontrado" })
+    }
+
+    if (!descricao || !quantidade_estoque || !valor || !categoria_id) {
+      return res.status(404).json({ mensagem: 'Os campos descricao, quantidade_estoque, valor e categoria_id são obrigatórios' })
+    }
+    if (categoria_id <= 0 || categoria_id > 8) {
+      return res.status(404).json({ mensagem: "A categoria informada não existe!" })
+    }
+
+    const atualizandoProduto = await knex('produtos').update({ descricao, quantidade_estoque, valor, categoria_id }).where({ id }).returning('*');
+
+    return res.status(200).json(atualizandoProduto);
+  } catch (error) {
+    return res.status(500).json({ mensagem: `Erro interno: ${error.menssage}` })
   }
 }
 
 
-
 module.exports = {
   cadastrarProduto,
-    listarProduto,
-    deletarProdutoPorId
+  listarProduto,
+  deletarProdutoPorId,
+  editarDadosProduto
 }
--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const { Router } = require('express')
 const { listarCategorias } = require('./controladores/categorias')
-const { cadastroDeUsuario, editarUsuario,detalharDadosPerfilUsuario} = require('./controladores/usuarios')
-const { listarProduto, cadastrarProduto } = require('./controladores/produtos')
+const { cadastroDeUsuario, editarUsuario, detalharDadosPerfilUsuario } = require('./controladores/usuarios')
+const { listarProduto, cadastrarProduto, editarDadosProduto } = require('./controladores/produtos')
 const login = require('./controladores/login')
 const autenticarUsuario = require('./filtros/autenticacao_de_usuario')
 const { schemaUsuario, schemaLogin, schemaProdutos } = require('./validações/schemaUsuario')
@@ -35,6 +35,7 @@ rotas.get('/cliente/:id', detalharCliente)
 rotas.post('/produto', validarSchema(schemaProdutos), cadastrarProduto)
 rotas.get('/produto', listarProduto)
 rotas.delete('/produto/:id', deletarProdutoPorId)
+rotas.put('/produto/:id', editarDadosProduto)
 
 
 module.exports = rotas
