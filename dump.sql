11bd9fdee79777c508b361b1233a23754621bae1
William Diangellis <diangellis2014@gmail.com>
2023-04-26 21:03:10 -0300

    Criação das tabelas do banco de dados pdv

--- dump.sql
+++ dump.sql
@@ -17,3 +17,26 @@ create table
 insert into
     categorias (descricao)
 values ('Informática'), ('Celulares'), ('Beleza e Perfumaria'), ('Mercado'), ('Livros e Papelaria'), ('Moda'), ('Bebê'), ('Games');
+
+create table
+    produtos (
+        id serial primary key,
+        descricao varchar(100) not null,
+        quantidade_estoque integer not null,
+        valor integer not null,
+        categoria_id integer not null references categorias(id)
+    );
+
+create table
+    clientes (
+        id serial primary key,
+        nome varchar(50) not null,
+        email text unique not null,
+        cpf char(11) unique not null,
+        cep char(8) not null,
+        rua varchar(50) not null,
+        numero varchar(20) not null,
+        bairro varchar(50) not null,
+        cidade varchar(50) not null,
+        estado varchar(2) not null
+    );
