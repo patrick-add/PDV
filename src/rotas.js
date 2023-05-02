78b1361e8af2e14dd20037a6d6f05617868e9c43
William Diangellis <diangellis2014@gmail.com>
2023-05-02 17:23:37 -0300

    Criação do endpoint detalharProduto

--- src/rotas.js
+++ src/rotas.js
@@ -1,14 +1,28 @@
 const path = require('path')
 const { Router } = require('express')
 const { listarCategorias } = require('./controladores/categorias')
-const { cadastroDeUsuario, editarUsuario, detalharDadosPerfilUsuario } = require('./controladores/usuarios')
-const { listarProduto, cadastrarProduto, editarDadosProduto } = require('./controladores/produtos')
+const {
+  cadastroDeUsuario,
+  editarUsuario,
+  detalharDadosPerfilUsuario
+} = require('./controladores/usuarios')
+const {
+  listarProduto,
+  cadastrarProduto,
+  editarDadosProduto,
+  detalharProduto
+} = require('./controladores/produtos')
 const login = require('./controladores/login')
 const autenticarUsuario = require('./filtros/autenticacao_de_usuario')
 const { schemaUsuario, schemaLogin, schemaProdutos } = require('./validações/schemaUsuario')
 const validarSchema = require('./intermediarios/validacarSchema')
 const { deletarProdutoPorId } = require('./controladores/produtos')
-const { detalharCliente, listarClientes, cadastrarCliente, editarDadosCliente } = require('./controladores/clientes')
+const {
+  detalharCliente,
+  listarClientes,
+  cadastrarCliente,
+  editarDadosCliente
+} = require('./controladores/clientes')
 const rotas = Router()
 
 rotas.get('/', (req, res) => {
@@ -38,5 +52,6 @@ rotas.post('/produto', validarSchema(schemaProdutos), cadastrarProduto)
 rotas.get('/produto', listarProduto)
 rotas.delete('/produto/:id', deletarProdutoPorId)
 rotas.put('/produto/:id', editarDadosProduto)
+rotas.get('/produto/:id', detalharProduto)
 
 module.exports = rotas
